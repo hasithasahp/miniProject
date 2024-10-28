e36cff9ae1930e8d5aa8e536de95b1bf2147de0a
Hasitha Samarasinghe <hasitha.h45h@gmail.com>
2024-10-28 21:26:46 +0530

    Question 3 database

---
+++ Question 03/studentdetails.sql
@@ -0,0 +1,8 @@
+INSERT INTO `horizonstudents` 
+(`id`, `index_no`, `first_name`, `last_name`, `city`, `district`, `province`, `email_address`, `mobile_number`) 
+VALUES 
+(NULL, 'IT2022123', 'Ishan', 'Sudesh', 'Kandy', 'Kandy', 'Central', 'ishan.sudesh@my.horizon.lk', '+94762212345'),
+(NULL, 'IT2022234', 'Hasitha', 'Samarasinghe', 'Ganewatta', 'Kandy', 'Western', 'hasitha.samarasinghe@my.horizon.lk', '+94762212234'),
+(NULL, 'IT2022345', 'Krushani', 'Perera', 'Kaduwela', 'Colombo', 'North Western', 'krushani.perera@my.horizon.lk', '+94762212345'),
+(NULL, 'IT2022456', 'Janith', 'Mahagamage', 'Norochcholai', 'Puttalam', 'North Western', 'janith.mahagamage@my.horizon.lk', '+94762212456'),
+(NULL, 'IT2022567', 'Niroshana', 'Roshan', 'Polonnaruwa', 'Polonnaruwa', 'North Central', 'niroshana.roshan@my.horizon.lk', '+94762212567');
